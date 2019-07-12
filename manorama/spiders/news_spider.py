7e857298eb424a11983a010321e887ce07edcdb4
JACOBIN-SCTCS <jacobja3@gmail.com>
2019-07-12 19:39:20 +0530

    Initial Commit

---
+++ manorama/spiders/news_spider.py
@@ -0,0 +1,59 @@
+import scrapy
+import json
+
+
+
+
+class NewsSpider(scrapy.Spider):
+    name = "news"
+    
+    sites = ['astrology','news','sports']
+
+    def gen_url(self):
+
+        url = []
+        for site in self.sites:
+            url.append('https://www.manoramaonline.com/content/mm/mo/'+site+'/_jcr_content/mm-section-top-left/channelallstories.results.15.4.json')
+        return url   
+
+
+    def start_requests(self):
+
+        urls = self.gen_url()
+
+        for url in urls : 
+            yield scrapy.Request(url,callback=self.parse)
+
+   
+   
+
+    def parse(self,response):
+        json_data = str(json.loads(response.text)['articleList'])
+        json_format = json.loads(json_data)
+        next_page = response.urljoin(json_format[0]['url'])
+        yield scrapy.Request(next_page, callback=self.get_data)
+                
+    def get_data(self,response): 
+        headline = response.css('div.story-header h1.story-headline::text').get()
+        category = response.url.split("/")[3]
+        text = '.'.join(response.css('div.rte-article p::text').getall())
+        with open("res.txt","a") as f : 
+            f.write('\n{\n \"category\" : ' + category + 
+                '\n\"headline\" : ' + headline + 
+                '\n\"text\" : ' + text +
+                '\n} '
+                
+                )
+                
+                
+        
+
+       
+
+       
+
+    
+        
+            
+
+    
